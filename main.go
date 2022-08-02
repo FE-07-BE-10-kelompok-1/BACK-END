1d42bc933faaaa41a6fa86d8399ad154b1a9be6b
Atha Dhaiffathin <athadhaiffathin@gmail.com>
2022-08-02 10:41:14 +0700

    books complete

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bookstore/config"
 	"bookstore/factory"
+	"bookstore/infrastructure/aws/s3"
 	"bookstore/infrastructure/database/mysql"
 	"fmt"
 
@@ -13,10 +14,17 @@ func main() {
 	cfg := config.GetConfig()
 	db := mysql.InitDB(cfg)
 	mysql.MigrateData(db)
+	session := s3.ConnectAws(cfg)
 
-	fmt.Println(cfg)
 	e := echo.New()
 	factory.InitFactory(e, db)
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set("session", session)
+			c.Set("bucket", cfg.BUCKET_NAME)
+			return next(c)
+		}
+	})
 
 	fmt.Println("Menjalankan program...")
 	dsn := fmt.Sprintf(":%d", config.SERVERPORT)
